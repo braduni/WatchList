5a58ce96ca43b50dfa48eb00dca4790e2dfeb90f
Brad <bradley1@protonmail.com>
2023-08-03 18:49:40 +0100

    Add Movie model

---
+++ WatchList/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace WatchList.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Director { get; set; } = string.Empty;
+    }
+}
