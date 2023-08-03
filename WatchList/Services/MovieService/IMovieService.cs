2daefee110700fd7096989039e7fa54d30fa3dcd
Brad <bradley1@protonmail.com>
2023-08-03 19:21:00 +0100

    Add movie interface and service

---
+++ WatchList/Services/MovieService/IMovieService.cs
@@ -0,0 +1,11 @@
+﻿namespace WatchList.Services.MovieService
+{
+    public interface IMovieService
+    {
+        Task<List<Movie>> GetAllMovies();
+        Task<Movie?> GetMovieById(int id);
+        Task<List<Movie>> AddMovie(Movie request);
+        Task<List<Movie>?> UpdateMovie(int id, Movie request);
+        Task<List<Movie>?> DeleteMovie(int id);
+    }
+}
---
+++ WatchList/Services/MovieService/MovieService.cs
@@ -0,0 +1,30 @@
+﻿namespace WatchList.Services.MovieService
+{
+    public class MovieService : IMovieService
+    {
+        public Task<List<Movie>> AddMovie(Movie request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Movie>?> DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Movie>> GetAllMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie?> GetMovieById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Movie>?> UpdateMovie(int id, Movie request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
