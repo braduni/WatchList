24fb4fe9f590157b4430cb7d8481496e6c5cb1d4
Brad <bradley1@protonmail.com>
2023-08-03 19:33:28 +0100

    implemented movie controller with movie service

--- WatchList/Controllers/MovieController.cs
+++ WatchList/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WatchList.Services.MovieService;
 
 namespace WatchList.Controllers
 {
@@ -7,29 +8,38 @@ namespace WatchList.Controllers
     [ApiController]
     public class MovieController : ControllerBase
     {
+        private readonly IMovieService _movieService;
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<Movie>>> GetAllMovies()
         {
-            throw new NotImplementedException();
+            return await _movieService.GetAllMovies();
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Movie>> GetBookById(int id)
         {
-            throw new NotImplementedException();
+            var result = await _movieService.GetMovieById(id);
+            return Ok(result);
         }
 
         [HttpPost]
         public async Task<ActionResult<List<Movie>>> AddMovie(Movie movie)
         {
-            throw new NotImplementedException();
+            var result = await _movieService.AddMovie(movie);
+            return Ok(result);
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<List<Movie>>> UpdateMovie(int id, Movie movie)
         {
-            throw new NotImplementedException();
+            var result = await _movieService.UpdateMovie(id, movie);
+            return Ok(result);
         }
+
     }
 }
