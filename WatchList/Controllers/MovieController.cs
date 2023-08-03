3147c829ee6610d248a5d1882fbaf6bf5c3e600e
Brad <bradley1@protonmail.com>
2023-08-03 19:32:01 +0100

    add movie controller

---
+++ WatchList/Controllers/MovieController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WatchList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<Movie>>> GetAllMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetBookById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Movie>>> AddMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Movie>>> UpdateMovie(int id, Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
