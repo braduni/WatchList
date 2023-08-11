d8744dafaf490c37d4380285bbde66d3f6ead259
Brad <bradley1@protonmail.com>
2023-08-11 18:58:37 +0100

    Add generic crud interface and implementation

---
+++ WatchList/Services/Implementation/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+using WatchList.Services.Repository;
+
+namespace WatchList.Services.Implementation
+{
+    public abstract class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly DataContext _context;
+
+        public GenericRepository(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+        public IQueryable<T> FindAll()
+        {
+            return _context.Set<T>().AsNoTracking();
+        }
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression).AsNoTracking();
+        }
+        public void Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+    }
+}
